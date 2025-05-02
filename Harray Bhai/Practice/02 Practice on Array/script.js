8333eb425f851a6d036defd7d0a3b9207f150bae
Abhay-Kumar-03 <abhaychauhan8052@gmail.com>
2025-05-02 11:44:52 +0530

    add

--- Harray Bhai/Practice/02 Practice on Array/script.js
+++ Harray Bhai/Practice/02 Practice on Array/script.js
@@ -1,6 +1,22 @@
 /*
 // Question-1
 // create an array of number and take input from user and then add that number in array
+let arr = [1, 2, 3, 4, 5, 6, 7]
+        let a = prompt("enter a number")
+        a = Number.parseInt(a)
+        arr.push(a)
+        console.log(arr)
+
+
+
+// Question-2
+let arr = [1, 2, 3, 4, 5, 6, 7]
+do{
+    let a = prompt("enter number")
+    a = Number.parseInt(a)
+    arr.push(a)
+    console.log(arr)
+}while(a!=0)
 
 
 
@@ -24,10 +40,11 @@ console.log(d)
 
 */
 
+
 //Question-5
 // multiply of all number in arr
-let arr5 = [1, 2, 3, 4]
-let e = arr5.reduce((h1,h2)=>{
-    return h1*h2
-})
-console.log(e)
+let arr5 = [1, 2, 3, 4];
+let e = arr5.reduce((h1, h2) => {
+  return h1 * h2;
+});
+console.log(e);
